ccbc489f16c385a5db911f5be53b25c4fea4d57c
omar <omar.alshyokh@gmail.com>
2021-06-08 01:20:23 +0300

    Initial Commit

---
+++ uitest/uitestApp.swift
@@ -0,0 +1,17 @@
+//
+//  uitestApp.swift
+//  uitest
+//
+//  Created by omar on 6/8/21.
+//
+
+import SwiftUI
+
+@main
+struct uitestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
